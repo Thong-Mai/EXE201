e5256480d98db7532c64693ef14ef3c14974590c
Mai Nhat Thong <thongmnse181865@fpt.edu.vn>
2026-06-11 22:05:44 +0700

    Force IPv4 SMTP

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,16 +1,18 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "74.125.68.108", // IPv4 của Gmail SMTP
   port: 587,
   secure: false,
+
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
   },
+
   tls: {
     rejectUnauthorized: false
   }
