89e7aa91546b87009662bbef73cca666d3018bfb
Mai Nhat Thong <thongmnse181865@fpt.edu.vn>
2026-06-11 23:15:56 +0700

    new

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,21 +1,31 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import dns from "dns";
 
 dotenv.config();
 
+// Ưu tiên IPv4
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
-  host: "74.125.68.108", // IPv4 của Gmail SMTP
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
 
   auth: {
     user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD
+    pass: process.env.GMAIL_APP_PASSWORD,
   },
 
+  family: 4, // ép IPv4
+
   tls: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export default transporter;
